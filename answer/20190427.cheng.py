98b10ae6ebccb6e56671cb099723209deb658018
tcxbp9 <able77@126.com>
2019-04-28 10:03:05 +0800

    Update 20190427.cheng.py

--- answer/20190427.cheng.py
+++ answer/20190427.cheng.py
@@ -1,3 +1,6 @@
+
+# 第一问：输入n个分样本各自的均值和标准差（mu1,mu2…）和（sigma1,sigma2…），计算这些样本所组成的合样本的均值和标准差(mu,sigma)
+
 def sample_stats():
     
     import numpy as np
@@ -23,4 +26,25 @@ def sample_stats():
     return n_k,bar_k,sigma_k,n,xbar,xsigma
 
 sample_stats()
+
+# 第二问：输入一组样本数据，绘制这个数据均值的抽样分布直方图
+
+def sampling_dist(mean,std,n):
+    
+    xbar = []
+
+    for i in range(100):
+    
+        data = np.random.normal(mean,std,n) 
+        xbar = np.append(xbar,data.mean())
+        
+    mu = xbar.mean()
+    sigma = xbar.std()
+
+    return plt.hist(xbar,bins=20,color="k",
+                    alpha=0.5,rwidth=0.9,range=(50,54)),plt.title("n={0},mu={1:.2f},sigma={2:.2f}".format(n,mu,sigma))
+
+sampling_dist()
+
+
 
